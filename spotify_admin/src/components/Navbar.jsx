9140a3172e6208ea18ec5ee891201b6eb9c8d10f
nonso1123 <chinonsonwora@gmail.com>
2025-07-07 16:49:09 +0100

    commit

--- spotify_admin/src/components/Navbar.jsx
+++ spotify_admin/src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<div className="navbar w-full border-b-2 border-gray-800 px-5 sm:px-12 py-4 text-lg">
+		<div className="navbar w-full border-b-2 border-gray-800 flex justify-between items-center px-5 sm:px-12 py-4 text-lg">
 			<p>Admin panel</p>
+			<button className="bg-black py-1 px-3 rounded-full text-[15px] cursor-pointer text-white">
+				<a href="https://spotify-sy0g.onrender.com/">Home</a>
+			</button>
 		</div>
 	);
 };
